4d5b76882ab8c133ab6bef1a571fc349c7872eb0
Gagi <tohi1986@gmail.com>
2026-07-19 04:35:44 +0200

    added new page

---
+++ src/app/awesome/page.tsx
@@ -0,0 +1,11 @@
+
+export default function AwesomePage(){
+    return(
+        <main>
+            <h1 className="flex min-h-screen justify-center items-center text-amber-600 font-extrabold">
+                hello motherfucker
+            </h1>
+        </main>
+    )
+
+}
